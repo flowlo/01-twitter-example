49ace2fad491ed6ab9e260eda9a67d52029c35ca
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2017-02-05 11:54:48 +0100

    Implement Winston Markov

--- project.clj
+++ project.clj
@@ -11,7 +11,9 @@
                  [twitter-api "0.7.8"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.slf4j/slf4j-log4j12 "1.7.12"]
-                 [log4j/log4j "1.2.17"]]
+                 [log4j/log4j "1.2.17"]
+                 [ns-tracker "0.3.1"]]
    :main twitter-example.core
    :min-lein-version "2.0.0"
-   :plugins [[lein-environ "1.0.0"]])
+   :plugins [[lein-environ "1.0.0"]]
+   :repl {:host "0.0.0.0" :port 7888})
